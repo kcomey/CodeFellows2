32d1887a400dc3892dca7ebd34db8c11fda53b93
Kendall Comey <kendall.comey@gmail.com>
2015-04-27 14:20:13 -0700

    Day 1 Assignment

---
+++ DayOneAssignment/math_func.js
@@ -0,0 +1,9 @@
+var calc = require('./calc.js');
+
+console.log( 'Add: ' + calc.add(4, 2));
+
+console.log( 'Subtract: ' + calc.subtract(4, 2));
+
+console.log( 'Multiply: ' + calc.multiply(4, 2));
+
+console.log( 'Divide: ' + calc.divide(4, 2));
